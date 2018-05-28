a215b6758a4980e947d312c85baedbd1dcf7e6d4
Sören Lünsdorf <mail@lunsdorf.com>
2018-05-28 18:00:49 +0200

    feat: support JSX elements as children

--- src/react-await.tsx
+++ src/react-await.tsx
@@ -2,15 +2,15 @@ import * as React from "react";
 import PromiseProxy from "./promise-proxy";
 
 export type PendingProps = {
-  children: () => JSX.Element;
+  children: React.ReactNode | React.ReactNode[] | (() => JSX.Element);
 };
 
 export type RejectedProps<E = Error> = {
-  children: (reason: E) => JSX.Element;
+  children: React.ReactNode | React.ReactNode[] | ((reason: E) => JSX.Element);
 };
 
 export type ResolvedProps<T = any> = {
-  children: (result: T) => JSX.Element;
+  children: React.ReactNode | React.ReactNode[] | ((result: T) => JSX.Element);
 };
 
 export type AwaitProps<T = any> = {
@@ -37,10 +37,17 @@ const context = React.createContext<AwaitContext>({
   state: PromiseState.Pending,
 });
 
+function isFunction(children: AwaitProps["children"]): children is Function {
+  return "function" === typeof children;
+}
+
 export function Pending({ children }: PendingProps): JSX.Element {
   return (
     <context.Consumer>
-      {({ state }) => PromiseState.Pending === state ? children() : null}
+      {({ state }) => PromiseState.Pending === state
+        ? isFunction(children) ? children() : children
+        : null
+      }
     </context.Consumer>
   );
 }
@@ -48,7 +55,10 @@ export function Pending({ children }: PendingProps): JSX.Element {
 export function Resolved({ children }: ResolvedProps): JSX.Element {
   return (
     <context.Consumer>
-      {({ state, result }) => PromiseState.Resolved === state ? children(result) : null}
+      {({ state, result }) => PromiseState.Resolved === state
+        ? isFunction(children) ? children(result) : children
+        : null
+      }
     </context.Consumer>
   );
 }
@@ -56,7 +66,10 @@ export function Resolved({ children }: ResolvedProps): JSX.Element {
 export function Rejected({ children }: RejectedProps): JSX.Element {
   return (
     <context.Consumer>
-      {({ state, reason }) => PromiseState.Rejected === state ? children(reason as Error) : null}
+      {({ state, reason }) => PromiseState.Rejected === state
+        ? isFunction(children) ? children(reason as Error) : children
+        : null
+      }
     </context.Consumer>
   );
 }
